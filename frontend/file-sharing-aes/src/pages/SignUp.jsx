592a341e2cfd2cc6ee8696c73dee7b577bc5e032
BHAVESH <kallurbhavesh@gmail.com>
2025-03-14 13:07:39 +0530

    added chat application

--- frontend/file-sharing-aes/src/pages/SignUp.jsx
+++ frontend/file-sharing-aes/src/pages/SignUp.jsx
@@ -29,7 +29,7 @@ function SignUp() {
         e.preventDefault();
         setLoading(true);
         try {
-            // const response = await axios.post(`http://localhost:5000/api/auth/signup`, formData);
+            
             const response = await axios.post(`${import.meta.env.VITE_API_URL}/api/auth/signup`, formData);
             message.success(response.data.message);
             navigate('/login');
